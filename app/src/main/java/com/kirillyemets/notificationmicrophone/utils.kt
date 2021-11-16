2f053f82914e3816c9fb00e75fbf4a02cff76aeb
kirill <kirill.yemets@gmail.com>
2021-11-16 11:24:24 +0200

    Changed deferred with sharedFlow, fixing a bug

--- app/src/main/java/com/kirillyemets/notificationmicrophone/utils.kt
+++ app/src/main/java/com/kirillyemets/notificationmicrophone/utils.kt
@@ -1,25 +1,29 @@
 package com.kirillyemets.notificationmicrophone
 
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
-import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.first
 
 class SuspendableActivityResultLauncher<I, O>(
-    private val deferred: CompletableDeferred<O>,
+    private val sharedFlow: SharedFlow<O>,
     private val launcher: ActivityResultLauncher<I>
 ) {
 
     suspend fun launchAndAwait(input: I): O {
         launcher.launch(input)
-        return deferred.await()
+        return sharedFlow.first()
     }
 }
 
 fun <I, O> ComponentActivity.registerForSuspendableActivityResult(contract: ActivityResultContract<I, O>): SuspendableActivityResultLauncher<I, O> {
-    val deferred = CompletableDeferred<O>()
-    val launcher = registerForActivityResult(contract) {
-        deferred.complete(it)
+    val mutableSharedFlow = MutableSharedFlow<O>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
+    val launcher = registerForActivityResult(contract) { result ->
+        Log.d("kek", "tryEmit: ${mutableSharedFlow.tryEmit(result)}")
     }
-    return SuspendableActivityResultLauncher(deferred, launcher)
+    return SuspendableActivityResultLauncher(mutableSharedFlow, launcher)
 }
